8ccd03e239c4304f0ff94b7e1c3a7b78309ce432
Michael Blum <mblum@roguequery.com>
2013-07-25 15:09:41 -0500

    wired Card array to View
    TODO:
    
    - Create dealer and player hands
    - fix card_back

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var suits = ["hearts", "diamonds", "clubs", "spades"];
 var values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];   
-var ext = "png";
+var pre = "cards/";
+var ext = ".png";
 
 //lets create a card Object
 function Card(suit, value, filename) {
@@ -35,7 +36,8 @@ for(var suit = 0; suit < suits.length; suit++)
 {
   for(var value = 0; value < values.length; value++)
   {
-    var imageFile = values[value] + "_of_" + suits[suit] + "." + ext;
+    // var imageFile = pre + values[value] + "_of_" + suits[suit] + ext;
+    var imageFile = pre + "card_back.png";
     var card = new Card(suits[suit], values[value], imageFile);
     deck.push(card);
   }
@@ -45,33 +47,47 @@ for(var suit = 0; suit < suits.length; suit++)
 console.log(deck);
 shuffle(deck);
 
-var stage = new Kinetic.Stage({
-  container: 'container',
-  width: window.innerWidth,
-  height: window.innerHeight
-});
-var layer = new Kinetic.Layer();
+function loadImages(sources, callback) {
+  var images = new Array();
+  var loadedImages = 0;
+  var numImages = deck.length;
 
-var cardX = 20;
-var cardImages = new Array();
+  for(var index = 0; index < deck.length; index++) {
+    images[index] = new Image();
+    images[index].onload = function() {
+      if(++loadedImages >= numImages) {
+        callback(images);
+      }
+    };
+    images[index].src = sources[index];
+  }
+}
+var canvas = document.getElementById('myCanvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var context = canvas.getContext('2d');
+
+var sources = new Array();
 for(var index = 0; index < deck.length; index++)
 {
-  var cardView = new Image();
-  cardImages.push(cardView);
-  cardView.onload = function() {
-    var yoda = new Kinetic.Image({
-      x: cardX += 106 + 5,
-      y: 50,
-      image: cardView,
-      width: 106,
-      height: 118
-    });
-
-    // add the shape to the layer
-    layer.add(yoda);
-
-    // add the layer to the stage
-    stage.add(layer);
-  };
-  cardView.src = "cards/" + deck[index].imageFile;
+  sources.push(deck[index].imageFile);
 }
+
+loadImages(sources, function(images) {
+  var cardX = 20;
+  for(var index = 0; index < deck.length; index++)
+  {
+    context.drawImage(images[index], cardX += images[index].width*.25/3, 30, images[index].width*.25, images[index].height*.25);
+  }
+});
+
+function drawCards()
+{
+
+}
+// var stage = new Kinetic.Stage({
+//   container: 'container',
+//   width: window.innerWidth,
+//   height: window.innerHeight
+// });
+// var layer = new Kinetic.Layer();
