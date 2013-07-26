234157beadede66455c72464c0f2f84793a42993
mblum <mblum@roguequery.com>
2013-07-26 10:15:30 -0400

    created dealer and player hands

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,20 @@ var values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'ki
 var pre = "cards/";
 var ext = ".png";
 
+var playerHand = new Array();
+var dealerHand = new Array();
+
 //lets create a card Object
 function Card(suit, value, filename) {
   this.suit = suit;
   this.value = value;
-  this.imageFile = filename
-  this.flipped = false
+  this.imageFile = filename;
+  this.flipped = false;
+}
+
+function draw()
+{
+  return deck.pop();
 }
 
 function shuffle(deck)
@@ -45,56 +53,65 @@ for(var suit = 0; suit < suits.length; suit++)
 
 //lets print out the deck
 console.log(deck);
+//lets shuffle the deck
 shuffle(deck);
+//lets deal the cards
+playerHand.push(draw());
+playerHand.push(draw());
+dealerHand.push(draw());
+dealerHand.push(draw());
 
 function loadImages(sources, callback) {
   var cardImages = new Array();
   var loadedImages = 0;
-  var numImages = deck.length;
+  var numImages = 4;
 
-  for(var index = 0; index < deck.length; index++) {
+  var card_back = new Image();
+  card_back.src = pre + "card_back" + ext;
+
+  for(var index = 0; index < 4; index++) {
     cardImages[index] = new Image();
     cardImages[index].onload = function() {
       if(++loadedImages >= numImages) {
-        callback(cardImages);
+        callback(cardImages, card_back);
       }
     };
     cardImages[index].src = sources[index];
   }
 }
+
 var canvas = document.getElementById('myCanvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var context = canvas.getContext('2d');
 
+//load hands into View
 var sources = new Array();
-for(var index = 0; index < deck.length; index++)
-{
-  sources.push(deck[index].imageFile);
-}
+sources.push(dealerHand[0].imageFile);
+sources.push(dealerHand[1].imageFile);
+sources.push(playerHand[0].imageFile);
+sources.push(playerHand[1].imageFile);
 
 //card back
-loadImages(sources, function(cardImages) {
+loadImages(sources, function(cardImages, card_back) {
   var cardX = 20;
-  for(var index = 0; index < 2; index++)
+  var cardY = 30;
+  for(var index = 0; index < 4; index++)
   {
-    if(index%2 != 0){
-      var card_back = new Image();
-      card_back.src = pre + "card_back" + ext;
-      context.drawImage(card_back, cardX += cardImages[index].width*.25/3, 30, card_back.width*.25, card_back.height*.25);
+    if(index == 2)
+    {
+      cardX = 20;
+      cardY = 300;
+    }
+    if(index == 0 || index == 2){
+      context.drawImage(card_back, cardX += card_back.width*.25/3, cardY, card_back.width*.25, card_back.height*.25);
     }else{
-      context.drawImage(cardImages[index], cardX += cardImages[index].width*.25/3, 30, cardImages[index].width*.25, cardImages[index].height*.25);
+      context.drawImage(cardImages[index], cardX += cardImages[index].width*.25/3, cardY, cardImages[index].width*.25, cardImages[index].height*.25);
     }
   }
 });
 
-function drawCards()
+function getScore(hand)
 {
 
 }
-// var stage = new Kinetic.Stage({
-//   container: 'container',
-//   width: window.innerWidth,
-//   height: window.innerHeight
-// });
-// var layer = new Kinetic.Layer();
